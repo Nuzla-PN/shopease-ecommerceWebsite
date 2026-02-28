ca84f4b238f2a556750f34bfb5c1f83d095ca71c
Nuzla-PN <nuzlanazeer958575@gmail.com>
2026-02-27 13:09:45 -1200

    casesensitive updation in frontend

--- Frontend/src/pages/admin/SellerRequestList.jsx
+++ Frontend/src/pages/admin/SellerRequestList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from "react";
-import { getAllSellerAdmin } from "../../APIs/adminAPI.JS";
+import { getAllSellerAdmin } from "../../APIs/adminAPI.js";
 import { approveSellerAdmin,rejectSellerAdmin } from "../../APIs/adminAPI.js";
 
 const PAGE_SIZE = 10;
